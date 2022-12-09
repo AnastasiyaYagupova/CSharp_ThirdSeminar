51bd2772f489be480fb00915024a3beb90accd98
Anastasiya Yagupova <nastyayagupova@mail.ru>
2022-12-09 16:44:52 +0300

    add third example

---
+++ EXAMPLES/Example003/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 3
+Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. 
+Полученный массив вывести на экран.
+3 -> [1, 8, 27].
+5 -> [1, 8, 27, 64, 125]
+Указание: Вывод массива вынести в отдельную функцию.
+*/
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    int x = 0;
+    while(index < Length)
+    {
+        collection[index] = x+1;
+        index++;
+        x++;
+    }
+}
+void PrintArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while(index < Length)
+    {
+        collection[index] = ((int)Math.Pow(collection[index],3));
+        Console.WriteLine(collection[index]);
+        index++;
+    }
+}
+
+int N = new Random().Next(0,11);
+Console.WriteLine(N);
+
+int[] Array = new int [N];
+
+
+FillArray(Array);
+PrintArray(Array);
